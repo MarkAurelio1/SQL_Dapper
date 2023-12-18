aefa651654f7a1189ff44a0f0468d74a96c42651
Marcos Aurelio <markking8775@gmail.com>
2023-12-18 15:41:21 -0300

    Adições de novas funções

---
+++ Models/CareerItem.cs
@@ -0,0 +1,9 @@
+namespace SchoolDataAccess.Models
+{
+    public class CareerItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,8 @@
+namespace SchoolDataAccess.Models
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
